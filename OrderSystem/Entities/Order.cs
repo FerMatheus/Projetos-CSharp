d434b3ff054f923b0b4d6455ca58ec8466e60a94
Matheus Fernandes <freitas.matheus.profissional@outlook.com>
2022-05-22 15:52:24 -0300

    Classe Order criada, agrupando funcionalidades

---
+++ OrderSystem/Entities/Order.cs
@@ -0,0 +1,38 @@
+﻿using OrderSystem.Entities.Enums;
+
+namespace OrderSystem.Entities
+{
+    class Order
+    {
+        public Client Client { get; set; }
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; }
+
+        public Order()
+        {
+        }
+        public Order(OrderStatus status)
+        {
+            Status = status;
+            Date = DateTime.Now;
+        }
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double total = 0;
+            foreach (OrderItem item in Items)
+            {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+    }
+}
