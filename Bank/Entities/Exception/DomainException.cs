f7f3d1481d913d5bd1fdcbcaf0d1a2bce93f27f9
Matheus Fernandes <freitas.matheus.profissional@outlook.com>
2022-05-22 19:50:20 -0300

    Esse é um exemplo simples de tratameto de exceções

---
+++ Bank/Entities/Exception/DomainException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Bank.Entities.Exception
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        { 
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,34 @@
+﻿using Bank.Entities;
+using Bank.Entities.Exception;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Numer: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine());
+
+                Account x = new(number, name, balance, limit);
+                Console.Write("\nEnter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                x.Withdraw(amount);
+                Console.WriteLine($"New balance: {x.Balance.ToString("F2")}");
+            }
+            catch (DomainException ex)
+            {
+                Console.WriteLine($"Withdraw error: {ex.Message}");
+            }
+        }
+    }
+}
