2ba924917015ef41156ffef3a9b0620c04968adc
Matheus Fernandes <freitas.matheus.profissional@outlook.com>
2022-05-22 16:47:04 -0300

    Implementado um override no ToString da classe Ordem

--- OrderSystem/Entities/Order.cs
+++ OrderSystem/Entities/Order.cs
@@ -1,4 +1,5 @@
 ﻿using OrderSystem.Entities.Enums;
+using System.Text;
 
 namespace OrderSystem.Entities
 {
@@ -7,15 +8,16 @@ namespace OrderSystem.Entities
         public Client Client { get; set; }
         public DateTime Date { get; set; }
         public OrderStatus Status { get; set; }
-        public List<OrderItem> Items { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
 
         public Order()
         {
         }
-        public Order(OrderStatus status)
+        public Order(OrderStatus status, Client client)
         {
             Status = status;
             Date = DateTime.Now;
+            Client = client;
         }
         public void AddItem(OrderItem item)
         {
@@ -33,6 +35,20 @@ namespace OrderSystem.Entities
                 total += item.SubTotal();
             }
             return total;
+        }
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.AppendLine("ORDER SUMMARY");
+            str.AppendLine($"Order moment: {Date.ToString("dd/MM/yyyy HH:mm:ss")}");
+            str.AppendLine($"Client: {Client.Name} {Client.BirthDate} {Client.Email}");
+            str.AppendLine("Order items:");
+            foreach (OrderItem item in Items)
+            {
+                str.AppendLine($"{item.Item.Name}, ${item.Price.ToString("F2")}, Quantity: {item.Quantity}, SubTotal: ${item.SubTotal().ToString("F2")}");
+            }
+            str.AppendLine($"Total price: ${Total().ToString("F2")}");
+            return str.ToString();
         }
     }
 }
