9858d2787aae9c83e947002cf6b490b53bf9caf1
Matheus Fernandes <freitas.matheus.profissional@outlook.com>
2022-05-22 15:38:19 -0300

    Criado a classe cliente

---
+++ OrderSystem/Entities/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateOnly BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+        public Client(string name, DateOnly birthDate, string email)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Email = email;
+        }
+    }
+}
